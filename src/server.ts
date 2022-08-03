5f3eb1e211937cc082a4c8e45e13ee050d5bd8f9
Lucas Galvao <lucasmelo_galvao@hotmail.com>
2022-08-03 13:32:13 -0300

    adding procfile

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'reflect-metadata';
 import { AppDataSource } from './data-source';
 import routes from './shared/http';
@@ -6,6 +7,8 @@ import './shared/container';
 
 AppDataSource.initialize().then(() => {
     const app = express();
+
+    app.use(cors());
 
     app.use(express.json());
 
